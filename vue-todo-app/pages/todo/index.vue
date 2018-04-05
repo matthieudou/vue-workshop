7e79a0160e186b20b1d1cfe4e382757bdeb2f8ce
matthieudou <matthieu.doultremont@gmail.com>
2018-04-06 01:02:00 +0200

    add new item and reset

--- vue-todo-app/pages/todo/index.vue
+++ vue-todo-app/pages/todo/index.vue
@@ -3,7 +3,8 @@
   <div class="container w-full md:w-1/2 lg:w-1/4">
     <h1 class="font-hairline text-white font-sans pb-4 text-center">TODO's</h1>
     <div class="rounded border shadow bg-white overflow-hidden">
-      <div>
+      <div class="flex flex-col">
+        <input class="py-4 px-8 border-b" type="text" name="addItem" id="addItem" placeholder="Ajouter un élément" v-model="newItem" @keyup.enter="addItem" @keyup.esc="reset">
         <label v-for="(item, key) in items" :key="key" :for="key" class="py-4 px-8 bg-grey-lighter border-b flex items-center" :class="{'bg-green-lighter': item.checked}">
           <input class="mr-8" :id="key" type="checkbox" :name="key" :value="item.checked" v-model="item.checked">
           {{item.title}}
@@ -18,12 +19,22 @@
 export default {
   data () {
     return {
+      newItem: '',
       items: [
         {title: 'Du beurre', checked: true},
         {title: 'Du fromage', checked: false},
         {title: 'Du vin', checked: false},
         {title: 'Du pain', checked: false},
       ]
+    }
+  },
+  methods: {
+    addItem () {
+      this.items.push({title: this.newItem, checked: false})
+      this.reset()
+    },
+    reset () {
+      this.newItem = ''
     }
   }
 }
